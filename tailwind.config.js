60370d4ada14b25e7ee30b97373fd7ee456e33ca
Oscar Ivan Rojas Hincapie <oscarrojascs@gmail.com>
2024-12-19 16:17:10 -0500

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,20 +36,6 @@ module.exports = {
                 dark: '8px 8px 0px 0px #000',
                 darkInput: '4px 4px 0px 0px #000',
             },
-            animation: {
-                marquee: "marquee 25s linear infinite",
-                marquee2: "marquee2 25s linear infinite",
-            },
-            keyframes: {
-                marquee: {
-                    "0%": { transform: "translateX(0%)" },
-                    "100%": { transform: "translateX(-100%)" },
-                },
-                marquee2: {
-                    "0%": { transform: "translateX(100%)" },
-                    "100%": { transform: "translateX(0%)" },
-                },
-            },
         },
     },
     darkMode: 'class',
