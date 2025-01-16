876474d23c3616a542bc067139c869e3aad8c21c
eldesernauta <oscarrojascs@gmail.com>
2025-01-16 09:09:16 -0500

    fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import localFont from 'next/font/local'
-
-const France = localFont({
-    src: '../../src/fonts/France/France.ttf',
-    display: 'swap',
-})
 
 export default function Document() {
 
     return (
-        <Html lang="en" className={France.className}>
+        <Html lang="en">
             <Head>
                 <title>Oscar Rojas | Portfolio</title>
                 <link rel="icon" href="/favicon.ico" />
